992f19bb17fc1d2de547bbd064ce32ecb9a05d62
salmarakha <61853942+salmarakha@users.noreply.github.com>
2021-06-30 05:57:19 +0200

    refactor subcategory model

--- src/app/Models/subCategory.ts
+++ src/app/Models/subCategory.ts
@@ -1,6 +1,10 @@
 export interface SubCategory {
-  _id:any;
-  name:string;
-  photo:string;
-  categoryId:any;
+  _id: any;
+  name: string;
+  photo: string;
+  categoryId: {
+    _id: any,
+    name: string,
+    photo: string,
+  }
 }
